03442eef5f017186453184c70d03012eaff1e254
jwkcp <jaewoong.go@gmail.com>
2023-02-16 09:10:47 +0900

    Initial commit

---
+++ src/main/java/hello/spingmvc/SpingmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.spingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingmvcApplication.class, args);
+    }
+
+}
